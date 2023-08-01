b9d29427091f9255056c6e04c9c9235cda7af30b
andrei <dam195@yahoo.com>
2023-08-01 17:41:27 +0300

    deletes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXTAUTH_URL: "http://localhost:3000/",
-    NEXTAUTH_SECRET: "RaK5+5gCa2Mx8jq6KW/UAAIq/nLiTWZ7Tohkvzo8W3M=",
-    mongoUri:
-      "mongodb+srv://admin:UCUOVqKCS95Q7qVe@cluster0.jnb6d.mongodb.net/rose_dimat",
-  },
-
   experimental: {
     serverComponentsExternalPackages: ["mongoose", "bcryptjs"],
   },
